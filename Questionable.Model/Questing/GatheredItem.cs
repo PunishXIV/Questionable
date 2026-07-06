25eaba3bea704453dcb2f3cbcb11d317db6e4d4c
raitono <bpaintx2@gmail.com>
2026-07-06 14:55:46 -0400

    Add some comments and tests that clarify ItemsToGather Collectability and AlternateItemId deserialize to 0 intentionally instead of being nullable.

--- Questionable.Model/Questing/GatheredItem.cs
+++ Questionable.Model/Questing/GatheredItem.cs
@@ -3,7 +3,13 @@
 public sealed class GatheredItem
 {
     public uint ItemId { get; set; }
+    /// <summary>
+    /// For leves that allow you to gather two items with different chance percentage, this is the preferred item if the gathering chance is 100% (after buffs). May be omitted from quest path JSON and will intentionally deserialize to 0 instead of null.
+    /// </summary>
     public uint AlternativeItemId { get; set; }
     public int ItemCount { get; set; }
+    /// <summary>
+    /// May be omitted from quest path JSON and will intentionally deserialize to 0 instead of null.
+    /// </summary>
     public ushort Collectability { get; set; }
 }
