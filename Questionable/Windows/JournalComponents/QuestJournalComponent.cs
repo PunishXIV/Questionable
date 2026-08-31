9bd5ffb665b6ddaafd6d2895239fab699e2e3668
alydev <aly@aly.pet>
2026-08-31 23:06:53 +1000

    journal progress "show only blue quests"

--- Questionable/Windows/JournalComponents/QuestJournalComponent.cs
+++ Questionable/Windows/JournalComponents/QuestJournalComponent.cs
@@ -436,6 +436,9 @@ internal sealed class QuestJournalComponent
         if (filter.AvailableOnly && !questFunctions.IsReadyToAcceptQuest(questInfo.QuestId))
             return false;
 
+        if (filter.BlueOnly && ((QuestInfo)questInfo).AvailableIcon is uint icon && !Enumerable.Range(40, 10).Contains((int)icon % 100))
+            return false;
+
         if (filter.HideNoPaths &&
             (!questRegistry.TryGetQuest(questInfo.QuestId, out Quest? quest) || quest.Root.Disabled))
             return false;
@@ -468,6 +471,7 @@ internal sealed class QuestJournalComponent
     internal sealed class FilterConfiguration
     {
         public bool AvailableOnly;
+        public bool BlueOnly;
         public bool HideNoPaths;
         public bool HideCompleted;
         public bool HideUnobtainable;
@@ -476,14 +480,18 @@ internal sealed class QuestJournalComponent
 
         public bool AdvancedFiltersActive =>
             AvailableOnly ||
+            BlueOnly ||
             HideNoPaths ||
-            HideCompleted;
+            HideCompleted ||
+            HideUnobtainable ||
+            HideRepeatable;
 
         public FilterConfiguration WithoutName()
         {
             return new()
             {
                 AvailableOnly = AvailableOnly,
+                BlueOnly = BlueOnly,
                 HideNoPaths = HideNoPaths,
                 HideCompleted = HideCompleted,
                 HideUnobtainable = HideUnobtainable,
